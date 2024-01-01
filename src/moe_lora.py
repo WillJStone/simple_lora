2635599b8eddedbd5f548894b2b5b2ab6b291dbd
will.stone <will.stone@reeldata.ai>
2024-01-01 14:08:20 -0400

    redoing moe layer with better understanding

--- src/moe_lora.py
+++ src/moe_lora.py
@@ -6,6 +6,7 @@ from typing import List
 
 import torch
 from torch import nn
+from torch.nn import functional as F
 
 
 class LoraExpert(nn.Module):
@@ -23,33 +24,23 @@ class LoraExpert(nn.Module):
         return weights + self.A @ self.B * self.lora_alpha
 
 
-class MoLoraLayer(nn.Module):
-    """
-    Modified from Mistral's MoE implementation
-    """
-    def __init__(self, feed_forward: nn.Module, experts: List[nn.Module], gate: nn.Module, num_experts_per_tok: int):
+class MoeLayer(nn.Module):
+    def __init__(self, feed_forward: nn.Module, experts: List[nn.Module], gate: nn.Module, moe_args: MoeArgs):
         super().__init__()
         assert len(experts) > 0
-        self.feed_forward = feed_forward
         self.experts = nn.ModuleList(experts)
         self.gate = gate
-        self.num_experts_per_tok = num_experts_per_tok
+        self.feed_forward = feed_forward
+        self.args = moe_args
 
     def forward(self, inputs: torch.Tensor):
-        inputs_squashed = inputs.view(-1, inputs.shape[-1])
-        gate_logits = self.gate(inputs_squashed)
-        expert_weights, selected_experts = torch.topk(
-            gate_logits, self.args.num_experts_per_tok
-        )
-        expert_weights = nn.functional.softmax(
-            expert_weights,
-            dim=1,
-            dtype=torch.float,
-        ).type_as(inputs)
-        results = torch.zeros_like(inputs_squashed)
+        gate_logits = self.gate(inputs)
+        weights, selected_experts = torch.topk(gate_logits, self.args.num_experts_per_tok)
+        weights = F.softmax(weights, dim=1, dtype=torch.float).to(inputs.dtype)
+        results = torch.zeros_like(inputs)
         for i, expert in enumerate(self.experts):
             batch_idx, nth_expert = torch.where(selected_experts == i)
-            results[batch_idx] += expert_weights[batch_idx, nth_expert, None] * expert(
-                inputs_squashed[batch_idx]
+            results[batch_idx] += weights[batch_idx, nth_expert, None] * expert(
+                inputs[batch_idx]
             )
-        return results.view_as(inputs)
+        return results
